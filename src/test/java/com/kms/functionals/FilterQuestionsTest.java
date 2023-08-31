1f5ca35c3fb86a8a962032350c4e12df94cac057
quynhngo <quynhngo@kms-technology.com>
2023-08-31 10:57:26 +0700

    restructure project and edit some unclear code

--- src/test/java/com/kms/functionals/FilterQuestionsTest.java
+++ src/test/java/com/kms/functionals/FilterQuestionsTest.java
@@ -1,2 +1,20 @@
-package com.kms.functionals;public class FilterQuestionsTest {
+package com.kms.functionals;
+
+import com.kms.processings.FilterQuestions;
+import com.kms.utils.Reader;
+import com.kms.utils.JSONWriter;
+import org.junit.Test;
+
+
+
+public class FilterQuestionsTest {
+    @Test
+    public void main() {
+        final String DATA_PATH = "src/test/resources/testdata/input/DataCollection.csv";
+        final String TARGET_PATH = "src/test/resources/testdata/output/Output.json";
+
+        Reader.ReadCSV(DATA_PATH);
+        FilterQuestions.ProcessData();
+        JSONWriter.CreateJSON(TARGET_PATH);
+    }
 }
